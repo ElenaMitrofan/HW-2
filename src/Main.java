cc7f45cb324158f53123c02609f5639e92e6a9ff
ElenaMitrofan <114084990+ElenaMitrofan@users.noreply.github.com>
2022-09-23 14:39:22 +0300

    Задание 3: задачи 6, 7, 8

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        var friend = 19;
-        System.out.println(friend);
-        friend = friend + 2;
-        System.out.println(friend);
-        friend = friend / 7;
-        System.out.println(friend);
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog = frog + 4;
-        System.out.println(frog);
+        var weightOf1Boxer = 78.2;
+        System.out.println(weightOf1Boxer);
+        var weightOf2Boxer = 82.7;
+        System.out.println(weightOf2Boxer);
+        var boxersWeight = weightOf1Boxer + weightOf2Boxer;
+        System.out.println("Общий вес двух бойцов " + boxersWeight);
+        var weightDifference = weightOf2Boxer - weightOf1Boxer;
+        System.out.println("Разница между весами бойцов " + weightDifference + " кг");
+        var weightDifference2 = weightOf2Boxer % weightOf1Boxer;
+        System.out.println("Разница между весами бойцов " + weightDifference2 + " кг");
+        var workTime = 640;
+        System.out.println(workTime);
+        var EmployeeWorkTime = 8;
+        System.out.println(EmployeeWorkTime);
+        var employee = workTime / EmployeeWorkTime;
+        System.out.println("Всего работников в компании – " + employee + " человек");
+        employee = employee + 94;
+        System.out.println(employee);
+        var workTime2 = employee * 8;
+        System.out.println("Если в компании работает " + employee + " человека, то всего " + workTime2 + " часов работы может быть поделено между сотрудниками");
+
 
 
     }
